33b9b70047239956ca16e28f4d7e5c11bf0d4fff
Tushar Vengurlekar <tushaarv@gmail.com>
2016-10-06 20:03:31 +0530

    Updated DB Code To Accept Params

--- data.js
+++ data.js
@@ -17,17 +17,49 @@ con.connect(function(err){
 });
 
 // Read
-con.query('SELECT * FROM employees',function(err,rows){
-  if(err) throw err;
+// con.query('SELECT * FROM employees',function(err,rows){
+//   if(err) throw err;
+//
+//   console.log('Data received from Db:\n');
+//   console.log(rows);
+//
+//   for (var i = 0; i < rows.length; i++) {
+//     console.log(rows[i].name);
+//   };
+// });
 
-  console.log('Data received from Db:\n');
-  console.log(rows);
+exports.read = function(req, res) {
+    con.query('SELECT * FROM employees',function(err,rows){
+      if(err) {
+        res.send({'error':'An error has occurred'});
+        throw err;
+      }
+      else {
+        res.send(rows);
+      }
+    });
+};
 
-  for (var i = 0; i < rows.length; i++) {
-    console.log(rows[i].name);
-  };
+exports.readById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving user: ' + id);
 
-});
+    // db.collection('wines', function(err, collection) {
+    //     collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
+    //         res.send(item);
+    //     });
+    // });
+
+    con.query('SELECT * FROM employees Where ID = ?',[id], function(err,rows){
+      if(err) {
+        res.send({'error':'An error has occurred'});
+        throw err;
+      }
+      else {
+        res.send(rows);
+      }
+    });
+};
 
 // Create
 // var employee = { name: 'Haresh', password: 'password4', profession:'Sailor'};
@@ -36,6 +68,33 @@ con.query('SELECT * FROM employees',function(err,rows){
 //
 //   console.log('Last insert ID:', res.insertId);
 // });
+
+exports.create = function(req, res) {
+    var user = req.body;
+    console.log('Adding user: ' + JSON.stringify(user));
+
+    // db.collection('wines', function(err, collection) {
+    //     collection.insert(wine, {safe:true}, function(err, result) {
+    //         if (err) {
+    //             res.send({'error':'An error has occurred'});
+    //         } else {
+    //             console.log('Success: ' + JSON.stringify(result[0]));
+    //             res.send(result[0]);
+    //         }
+    //     });
+    //   });
+
+      con.query('INSERT INTO employees SET ?', employee, function(err,res){
+        if(err) {
+          // throw err;
+          res.send({'error':'An error has occurred'});
+        }
+        else {
+          res.send(result[0]);
+        }
+      });
+    }
+
 
 // Update
 // con.query(
@@ -48,6 +107,54 @@ con.query('SELECT * FROM employees',function(err,rows){
 //   }
 // );
 
+exports.updateWine = function(req, res) {
+    var id = req.params.id;
+    var user = req.body;
+
+    var input = JSON.parse(JSON.stringify(req.body));
+
+    console.log('Updating user: ' + id);
+    console.log(JSON.stringify(user));
+
+    var data = {
+
+            name    : input.name,
+            password   : input.password,
+            profession   : input.profession
+        };
+
+    // db.collection('wines', function(err, collection) {
+    //     collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
+    //         if (err) {
+    //             console.log('Error updating wine: ' + err);
+    //             res.send({'error':'An error has occurred'});
+    //         } else {
+    //             console.log('' + result + ' document(s) updated');
+    //             res.send(wine);
+    //         }
+    //     });
+    // });
+
+    con.query(
+      'UPDATE employees SET ? Where ID = ?',
+      [data, id],
+      function (err, result) {
+        if (err)
+        {
+            console.log('Error updating wine: ' + err);
+            res.send({'error':'An error has occurred'});
+            throw err;
+        }
+        else {
+            console.log('' + result + ' document(s) updated');
+            res.send(result[0]);
+        }
+      }
+    );
+}
+
+
+
 //Delete
 // con.query(
 //   'DELETE FROM employees WHERE id = ?',
@@ -59,17 +166,34 @@ con.query('SELECT * FROM employees',function(err,rows){
 //   }
 // );
 
-con.query('SELECT * FROM employees',function(err,rows){
-  if(err) throw err;
+exports.delete = function(req, res) {
+    var id = req.params.id;
+    console.log('Deleting user: ' + id);
 
-  console.log('Data received from Db:\n');
-  console.log(rows);
+    // db.collection('wines', function(err, collection) {
+    //     collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+    //         if (err) {
+    //             res.send({'error':'An error has occurred - ' + err});
+    //         } else {
+    //             console.log('' + result + ' document(s) deleted');
+    //             res.send(req.body);
+    //         }
+    //     });
+    // });
 
-  for (var i = 0; i < rows.length; i++) {
-    console.log(rows[i].name);
-  };
-
-});
+    con.query(
+      'DELETE FROM employees WHERE id = ?',[id], function (err, result) {
+        if (err) {
+          res.send({'error':'An error has occurred - ' + err});
+          throw err;
+        }
+        else {
+          console.log('Deleted ' + result.affectedRows + ' rows');
+          res.send(req.body);
+        }
+      }
+    );
+}
 
 con.end(function(err) {
   // The connection is terminated gracefully
