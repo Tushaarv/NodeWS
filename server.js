f53b5264718f8a0df974bd53b96837f02296524f
Tushar Vengurlekar <tushaarv@gmail.com>
2016-10-06 20:03:54 +0530

    Updated for WebService

--- server.js
+++ server.js
@@ -1,17 +1,44 @@
 var express = require('express');
 var app = express();
-var fs = require("fs");
+// var fs = require("fs"); // For Hardcoded Retreival
+// var users = require('./routes/data');
+var users = require('./data');
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( data );
-   });
-})
+// app.configure(function () {
+// app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+// app.use(express.bodyParser());
+// });
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
+logger = require('morgan');
+app.use(logger('dev'));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
 
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+
+app.get('/users', users.read);
+app.get('/user/:id', users.readById);
+app.post('/users', users.create);
+app.put('/users/:id', users.update);
+app.delete('/users/:id', users.delete);
+
+app.listen(8081);
+console.log('Listening on port 8081...');
+
+
+// Hardcoded Retreival
+// app.get('/listUsers', function (req, res) {
+//    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+//       console.log( data );
+//       res.end( data );
+//    });
+// })
+
+// For Hardcoded Retreival
+
+// var server = app.listen(8081, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+//
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
