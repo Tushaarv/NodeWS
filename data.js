9715233b8ff98c6c4832fe9fa3a8b147ac3820f4
Tushar Vengurlekar <tushaarv@gmail.com>
2016-10-07 12:57:14 +0530

    Fixed The Service

--- data.js
+++ data.js
@@ -1,202 +1,105 @@
-var mysql = require("mysql");
+var dbMysql = require("mysql");
 
 // First you need to create a connection to the db
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "bidchat123",
-  database: "NodeWS"
+var dbObject = dbMysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "bidchat123",
+    database: "NodeWS"
 });
 
-con.connect(function(err){
-  if(err){
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Connection established');
-});
-
-// Read
-// con.query('SELECT * FROM employees',function(err,rows){
-//   if(err) throw err;
-//
-//   console.log('Data received from Db:\n');
-//   console.log(rows);
-//
-//   for (var i = 0; i < rows.length; i++) {
-//     console.log(rows[i].name);
-//   };
-// });
-
-exports.read = function(req, res) {
-    con.query('SELECT * FROM employees',function(err,rows){
-      if(err) {
-        res.send({'error':'An error has occurred'});
-        // throw err;
-      }
-      else {
-        res.send(rows);
-      }
-    });
-};
-
-exports.readById = function(req, res) {
-    var id = req.params.id;
-    console.log('Retrieving user: ' + id);
-
-    // db.collection('wines', function(err, collection) {
-    //     collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-    //         res.send(item);
-    //     });
-    // });
-
-    con.query('SELECT * FROM employees Where ID = ?',[id], function(err,rows){
-      if(err) {
-        res.send({'error':'An error has occurred'});
-        throw err;
-      }
-      else {
-        res.send(rows);
-      }
-    });
-};
-
-// Create
-// var employee = { name: 'Haresh', password: 'password4', profession:'Sailor'};
-// con.query('INSERT INTO employees SET ?', employee, function(err,res){
-//   if(err) throw err;
-//
-//   console.log('Last insert ID:', res.insertId);
-// });
-
-exports.create = function(req, res) {
-    var user = req.body;
-    console.log('Adding user: ' + JSON.stringify(user));
-
-    // db.collection('wines', function(err, collection) {
-    //     collection.insert(wine, {safe:true}, function(err, result) {
-    //         if (err) {
-    //             res.send({'error':'An error has occurred'});
-    //         } else {
-    //             console.log('Success: ' + JSON.stringify(result[0]));
-    //             res.send(result[0]);
-    //         }
-    //     });
-    //   });
-
-      con.query('INSERT INTO employees SET ?', employee, function(err,res){
-        if(err) {
-          // throw err;
-          res.send({'error':'An error has occurred'});
-        }
-        else {
-          res.send(result[0]);
-        }
-      });
+dbObject.connect(function(err) {
+    if (err) {
+        console.log('Error connecting to Db');
+        return;
     }
+    console.log('Connection established');
+});
 
-
-// Update
-// con.query(
-//   'UPDATE employees SET profession = ? Where ID = ?',
-//   ["FisherMan", 13],
-//   function (err, result) {
-//     if (err) throw err;
-//
-//     console.log('Changed ' + result.changedRows + ' rows');
-//   }
-// );
-
-exports.update = function(req, res) {
-    var id = req.params.id;
-    var user = req.body;
-
-    var input = JSON.parse(JSON.stringify(req.body));
-
-    console.log('Updating user: ' + id);
-    console.log(JSON.stringify(user));
-
-    var data = {
-
-            name    : input.name,
-            password   : input.password,
-            profession   : input.profession
-        };
-
-    // db.collection('wines', function(err, collection) {
-    //     collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
-    //         if (err) {
-    //             console.log('Error updating wine: ' + err);
-    //             res.send({'error':'An error has occurred'});
-    //         } else {
-    //             console.log('' + result + ' document(s) updated');
-    //             res.send(wine);
-    //         }
-    //     });
-    // });
-
-    con.query(
-      'UPDATE employees SET ? Where ID = ?',
-      [data, id],
-      function (err, result) {
-        if (err)
-        {
-            console.log('Error updating wine: ' + err);
-            res.send({'error':'An error has occurred'});
-            throw err;
+// Read all rows in User Table
+exports.read = function(request, response) {
+    dbObject.query('SELECT * FROM employees', function(errorReceived, result) {
+        if (errorReceived) {
+            response.send({
+                'error': 'An error has occurred' + errorReceived
+            });
+        } else {
+            response.send(result);
         }
-        else {
-            console.log('' + result + ' document(s) updated');
-            res.send(result[0]);
+    });
+};
+
+// Read a row in User Table matching supplied id
+exports.readById = function(request, response) {
+    var requestObjectId = request.params.id;
+    console.log('Retrieving : ' + requestObjectId);
+    dbObject.query('SELECT * FROM employees Where ID = ?', [requestObjectId], function(errorReceived, result) {
+        if (errorReceived) {
+            response.send({
+                'error': 'An error has occurred \n' + errorReceived
+            });
+        } else {
+            response.send(result);
         }
-      }
+    });
+};
+
+// Saving a User
+exports.create = function(request, response) {
+    var requestObject = request.body;
+    console.log('Adding : ' + JSON.stringify(requestObject));
+    dbObject.query('INSERT INTO employees SET ?', requestObject, function(errorReceived, result) {
+        if (errorReceived) {
+            response.send({
+                'error': 'An error has occurred \n' + errorReceived
+            });
+        } else {
+            requestObject.id = result.insertId;
+            response.send(requestObject);
+        }
+    });
+};
+
+// Updating User
+exports.update = function(request, response) {
+    var requestObjectId = request.params.id;
+    var requestObject = request.body;
+
+    console.log('Updating : ' + requestObjectId);
+    console.log(JSON.stringify(requestObject));
+
+    dbObject.query(
+        'UPDATE employees SET ? Where ID = ?', [requestObject, requestObjectId],
+        function(errorReceived, result) {
+            if (errorReceived) {
+                console.log('Error updating User: ' + errorReceived);
+                response.send({
+                    'error': 'An error has occurred' + errorReceived
+                });
+            } else {
+                console.log('' + result + ' document(s) updated');
+                requestObject.id = requestObjectId;
+                response.send(requestObject);
+            }
+        }
     );
-}
+};
 
-
-
-//Delete
-// con.query(
-//   'DELETE FROM employees WHERE id = ?',
-//   [13],
-//   function (err, result) {
-//     if (err) throw err;
-//
-//     console.log('Deleted ' + result.affectedRows + ' rows');
-//   }
-// );
-
-exports.delete = function(req, res) {
-    var id = req.params.id;
-    console.log('Deleting user: ' + id);
-
-    // db.collection('wines', function(err, collection) {
-    //     collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-    //         if (err) {
-    //             res.send({'error':'An error has occurred - ' + err});
-    //         } else {
-    //             console.log('' + result + ' document(s) deleted');
-    //             res.send(req.body);
-    //         }
-    //     });
-    // });
-
-    con.query(
-      'DELETE FROM employees WHERE id = ?',[id], function (err, result) {
-        if (err) {
-          res.send({'error':'An error has occurred - ' + err});
-          throw err;
+// Delete User
+exports.delete = function(request, response) {
+    var requestObjectId = request.params.id;
+    console.log('Deleting : ' + requestObjectId);
+    dbObject.query(
+        'DELETE FROM employees WHERE id = ?', [requestObjectId],
+        function(errorReceived, result) {
+            if (errorReceived) {
+                response.send({
+                    'error': 'An error has occurred - ' + errorReceived
+                });
+            } else {
+                console.log('Deleted ' + result.affectedRows + ' rows');
+                response.send(null);
+            }
         }
-        else {
-          console.log('Deleted ' + result.affectedRows + ' rows');
-          res.send(req.body);
-        }
-      }
     );
-}
-
-// con.end(function(err) {
-//   // The connection is terminated gracefully
-//   // Ensures all previously enqueued queries are still
-//   // before sending a COM_QUIT packet to the MySQL server.
-// });
+};
--- server.js
+++ server.js
@@ -1,44 +1,22 @@
 var express = require('express');
-var app = express();
-// var fs = require("fs"); // For Hardcoded Retreival
-// var users = require('./routes/data');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+// Object For Database Transactions
 var users = require('./data');
 
-// app.configure(function () {
-// app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-// app.use(express.bodyParser());
-// });
+var app = express();
 
-logger = require('morgan');
 app.use(logger('dev'));
-var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded());
 
-
+// User Transaction
 app.get('/users', users.read);
-app.get('/user/:id', users.readById);
+app.get('/users/:id', users.readById);
 app.post('/users', users.create);
 app.put('/users/:id', users.update);
 app.delete('/users/:id', users.delete);
 
 app.listen(8081);
 console.log('Listening on port 8081...');
-
-
-// Hardcoded Retreival
-// app.get('/listUsers', function (req, res) {
-//    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-//       console.log( data );
-//       res.end( data );
-//    });
-// })
-
-// For Hardcoded Retreival
-
-// var server = app.listen(8081, function () {
-//    var host = server.address().address
-//    var port = server.address().port
-//
-//    console.log("Example app listening at http://%s:%s", host, port)
-// })
