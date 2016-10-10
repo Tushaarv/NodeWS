a12c55dc83234d852617f141a86d1d0e78e9cbda
Tushar Vengurlekar <tushaarv@gmail.com>
2016-10-10 15:00:03 +0530

    Updated Route Names

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 // Object For Database Transactions
-var users = require('./data');
+var employees = require('./data');
 
 var app = express();
 
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded());
 
 // User Transaction
-app.get('/users', users.read);
-app.get('/users/:id', users.readById);
-app.post('/users', users.create);
-app.put('/users/:id', users.update);
-app.delete('/users/:id', users.delete);
+app.get('/employees', employees.read);
+app.get('/employees/:id', employees.readById);
+app.post('/employees', employees.create);
+app.put('/employees/:id', employees.update);
+app.delete('/employees/:id', employees.delete);
 
 app.listen(8081);
 console.log('Listening on port 8081...');
